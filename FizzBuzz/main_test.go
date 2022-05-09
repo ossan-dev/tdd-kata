93aec4d638bbec6784a258de0d1203b43c239633
Ivan Pesenti <ivan.pesenti.dev@gmail.com>
2022-05-09 12:41:24 +0200

    FizzBuzz - red

--- FizzBuzz/main_test.go
+++ FizzBuzz/main_test.go
@@ -38,3 +38,14 @@ func TestFizzBuzz_ShouldReturnFizz_WhenSixIsPassed(t *testing.T) {
 	// assert
 	assert.Equal(t, "Fizz", got)
 }
+
+func TestFizzBuzz_ShouldReturnBuzz_WhenTenIsPassed(t *testing.T) {
+	// arrange
+	intArg := 10
+
+	// act
+	got := FizzBuzz(intArg)
+
+	// assert
+	assert.Equal(t, "Buzz", got)
+}
