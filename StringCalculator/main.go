e7ace0164ff3766176245a7b5aad9b0e2ce439d2
Ivan Pesenti <ivan.pesenti.dev@gmail.com>
2022-05-09 17:26:56 +0200

    String Calc - green

--- StringCalculator/main.go
+++ StringCalculator/main.go
@@ -13,11 +13,23 @@ func Add(numbers string) int {
 		numToReturn, _ := strconv.Atoi(numbers)
 		return numToReturn
 	}
+	if len(numbers) == 3 {
+		num1Str := strings.Split(numbers, ",")[0]
+		num2Str := strings.Split(numbers, ",")[1]
+		num2, _ := strconv.Atoi(num2Str)
+		num1, _ := strconv.Atoi(num1Str)
+		return num1 + num2
+	}
+
 	num1Str := strings.Split(numbers, ",")[0]
 	num2Str := strings.Split(numbers, ",")[1]
+	num3Str := strings.Split(numbers, ",")[2]
+	num4Str := strings.Split(numbers, ",")[3]
 	num1, _ := strconv.Atoi(num1Str)
 	num2, _ := strconv.Atoi(num2Str)
-	return num1 + num2
+	num3, _ := strconv.Atoi(num3Str)
+	num4, _ := strconv.Atoi(num4Str)
+	return num1 + num2 + num3 + num4
 }
 
 func main() {
