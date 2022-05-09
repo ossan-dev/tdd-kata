3d00d4a33a2788ae9064926205ce6db43dcc886e
Ivan Pesenti <ivan.pesenti.dev@gmail.com>
2022-05-09 16:58:30 +0200

    StringCalculator - red

--- StringCalculator/main_test.go
+++ StringCalculator/main_test.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAdd_ShouldReturnZero_WhenStringEmptyIsPassed(t *testing.T) {
+	inputNumbers := ""
+
+	got := Add(inputNumbers)
+
+	assert.Equal(t, 0, got)
+}
