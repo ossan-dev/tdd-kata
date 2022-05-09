d7466759b4e78a154df7909f20b1e2ab0a0b7017
Ivan Pesenti <ivan.pesenti.dev@gmail.com>
2022-05-09 17:55:03 +0200

    StringCalc - green

--- StringCalculator/main.go
+++ StringCalculator/main.go
@@ -17,7 +17,7 @@ func Add(numbers string) (int, error) {
 
 	sum := 0
 	for _, line := range strings.Split(numbers, "\n") {
-		if line[len(line)-1] == ',' {
+		if _, err := strconv.Atoi(string(line[len(line)-1])); err != nil {
 			return 0, fmt.Errorf("the line %q terminates with an unallowed character", line)
 		}
 		for _, number := range strings.Split(line, ",") {
