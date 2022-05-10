377d2db84b4a5b239ed0a13f191b03dd4f0904b1
Ivan Pesenti <ivan.pesenti.dev@gmail.com>
2022-05-10 09:26:36 +0200

    Pass - red

--- PassValidator/main_test.go
+++ PassValidator/main_test.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestValidate_ShouldGetErr_WhenPassLenIsLessThanEight(t *testing.T) {
+	pass := "134567"
+
+	got, err := Validate(pass)
+
+	assert.Equal(t, false, got)
+	assert.Equal(t, "Password must be at least 8 characters", err.Error())
+}
