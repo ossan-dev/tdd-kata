df0365f38edc619a5c300cdf681a209a87c3d0ec
Ivan Pesenti <ivan.pesenti.dev@gmail.com>
2022-05-10 10:13:35 +0200

    Pass - green

--- PassValidator/main.go
+++ PassValidator/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"regexp"
 )
 
@@ -11,14 +12,16 @@ const (
 )
 
 func Validate(pass string) (bool, error) {
-	if len(pass) < 8 {
-		return false, errors.New(TOO_SHORT)
-	}
-
 	r, _ := regexp.Compile(`[\d]`)
 	numOfDigits := len(r.FindAllString(pass, -1))
+	if len(pass) < 8 && numOfDigits < 2 {
+		return false, fmt.Errorf("%v\n%v", TOO_SHORT, TOO_FEW_DIGITS)
+	}
 	if numOfDigits < 2 {
 		return false, errors.New(TOO_FEW_DIGITS)
+	}
+	if len(pass) < 8 {
+		return false, errors.New(TOO_SHORT)
 	}
 	return true, nil
 }
