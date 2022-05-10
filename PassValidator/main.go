a73ed8b8786df7b03513a82527fcb263bfd15d6e
Ivan Pesenti <ivan.pesenti.dev@gmail.com>
2022-05-10 09:53:25 +0200

    Pass - green

--- PassValidator/main.go
+++ PassValidator/main.go
@@ -1,12 +1,24 @@
 package main
 
-import "errors"
+import (
+	"errors"
+	"unicode"
+)
 
 const TOO_SHORT = "password must be at least 8 characters"
 
 func Validate(pass string) (bool, error) {
 	if len(pass) < 8 {
 		return false, errors.New(TOO_SHORT)
+	}
+	numbersInPass := 0
+	for _, v := range pass {
+		if unicode.IsDigit(v) {
+			numbersInPass++
+		}
+	}
+	if numbersInPass < 2 {
+		return false, errors.New("the password must contain at least 2 numbers")
 	}
 	return true, nil
 }
