c6d2f399b37cc1737c6a65ed5dfb7f0844f70c9a
Ivan Pesenti <ivan.pesenti.dev@gmail.com>
2022-09-20 17:11:32 +0200

    Search - red

--- SearchCity/main_test.go
+++ SearchCity/main_test.go
@@ -17,13 +17,18 @@ func TestSearchCity(t *testing.T) {
 			input:                  "a",
 			cities_to_be_in_result: []string{},
 		},
+		{
+			name:                   "MustReturnCitiesStartingWithTheInput_WhenInputIsLongerThanTwoChars",
+			input:                  "Va",
+			cities_to_be_in_result: []string{"Valencia", "Vancouver"},
+		},
 	}
 	for _, tt := range test_suite {
 		t.Run(tt.name, func(t *testing.T) {
 			got := SearchCity(tt.input)
 			for _, v := range tt.cities_to_be_in_result {
-				if assert.NotContains(t, got, v) {
-					t.Errorf("expected %q to be present in result which contains %v", v, got)
+				if !assert.Contains(t, got, v) {
+					t.Errorf("expected %q to be present in result which contains %q", v, got)
 				}
 			}
 		})
