21b3a0c3204ec83a1fa9a39d5c7914e2fe67f2b2
Ivan Pesenti <ivan.pesenti.dev@gmail.com>
2022-09-20 16:51:18 +0200

    Search - initial commit

---
+++ SearchCity/main.go
@@ -0,0 +1,3 @@
+package searchcity
+
+var cities = []string{"Paris", "Budapest", "Skopje", "Rotterdam", "Valencia", "Vancouver", "Amsterdam", "Vienna", "Sydney", "New York City", "London", "Bangkok", "Hong Kong", "Dubai", "Rome", "Istanbul"}
