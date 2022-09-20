e353a9b556f1f9a3439c71bf9a963be81ced59bf
Ivan Pesenti <ivan.pesenti.dev@gmail.com>
2022-09-20 17:11:41 +0200

    Search - green

--- SearchCity/main.go
+++ SearchCity/main.go
@@ -1,7 +1,17 @@
 package searchcity
 
+import "strings"
+
 var cities = []string{"Paris", "Budapest", "Skopje", "Rotterdam", "Valencia", "Vancouver", "Amsterdam", "Vienna", "Sydney", "New York City", "London", "Bangkok", "Hong Kong", "Dubai", "Rome", "Istanbul"}
 
 func SearchCity(input string) (result []string) {
+	if len(input) < 2 {
+		return
+	}
+	for _, v := range cities {
+		if strings.HasPrefix(v, input) {
+			result = append(result, v)
+		}
+	}
 	return
 }
