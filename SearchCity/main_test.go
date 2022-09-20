9274ff311bf19d78a5b5d84b067325a97e27e583
Ivan Pesenti <ivan.pesenti.dev@gmail.com>
2022-09-20 17:01:50 +0200

    Search - refactor

--- SearchCity/main_test.go
+++ SearchCity/main_test.go
@@ -7,6 +7,25 @@ import (
 )
 
 func TestSearchCity(t *testing.T) {
-	got := SearchCity("a")
-	assert.Empty(t, got)
+	test_suite := []struct {
+		name                   string
+		input                  string
+		cities_to_be_in_result []string
+	}{
+		{
+			name:                   "MustBeEmpty_WhenLessThanTwoCharsArePassed",
+			input:                  "a",
+			cities_to_be_in_result: []string{},
+		},
+	}
+	for _, tt := range test_suite {
+		t.Run(tt.name, func(t *testing.T) {
+			got := SearchCity(tt.input)
+			for _, v := range tt.cities_to_be_in_result {
+				if assert.NotContains(t, got, v) {
+					t.Errorf("expected %q to be present in result which contains %v", v, got)
+				}
+			}
+		})
+	}
 }
