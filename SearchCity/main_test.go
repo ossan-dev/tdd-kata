ce97cc7dffe36964510a6fc95031055c058ca241
Ivan Pesenti <ivan.pesenti.dev@gmail.com>
2022-09-20 16:22:18 +0200

    Search - red

--- SearchCity/main_test.go
+++ SearchCity/main_test.go
@@ -1,1 +1,12 @@
 package searchcity
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSearchCity(t *testing.T) {
+	got := SearchCity("st")
+	assert.Contains(t, got, "st")
+}
