17fe45586351082d037aaaf72c6aafe93e206919
Ivan Pesenti <ivan.pesenti.dev@gmail.com>
2022-09-20 16:39:51 +0200

    Search - green

--- SearchCity/main.go
+++ SearchCity/main.go
@@ -5,10 +5,13 @@ import "strings"
 var cities = []string{"Paris", "Budapest", "Skopje", "Rotterdam", "Valencia", "Vancouver", "Amsterdam", "Vienna", "Sydney", "New York City", "London", "Bangkok", "Hong Kong", "Dubai", "Rome", "Istanbul"}
 
 func SearchCity(input string) (result []string) {
+	if len(input) < 2 {
+		return
+	}
 	for _, v := range cities {
 		if strings.Contains(v, input) {
 			result = append(result, v)
 		}
 	}
-	return result
+	return
 }
