431f4eeb2caa96083db6b7089121be049da7b20a
Ivan Pesenti <ivan.pesenti.dev@gmail.com>
2022-09-20 16:54:21 +0200

    Search - green

--- SearchCity/main.go
+++ SearchCity/main.go
@@ -1,3 +1,7 @@
 package searchcity
 
 var cities = []string{"Paris", "Budapest", "Skopje", "Rotterdam", "Valencia", "Vancouver", "Amsterdam", "Vienna", "Sydney", "New York City", "London", "Bangkok", "Hong Kong", "Dubai", "Rome", "Istanbul"}
+
+func SearchCity(input string) (result []string) {
+	return
+}
