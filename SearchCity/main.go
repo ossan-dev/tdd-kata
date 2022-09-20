2a9c62dc77760d19bceeb4025570126d63e40603
Ivan Pesenti <ivan.pesenti.dev@gmail.com>
2022-09-20 16:29:49 +0200

    Search - refactor

--- SearchCity/main.go
+++ SearchCity/main.go
@@ -1,5 +1,14 @@
 package searchcity
 
+import "strings"
+
+var cities = []string{"Paris", "Budapest", "Skopje", "Rotterdam", "Valencia", "Vancouver", "Amsterdam", "Vienna", "Sydney", "New York City", "London", "Bangkok", "Hong Kong", "Dubai", "Rome", "Istanbul"}
+
 func SearchCity(input string) (result []string) {
+	for _, v := range cities {
+		if strings.Contains(v, input) {
+			result = append(result, v)
+		}
+	}
 	return []string{input}
 }
