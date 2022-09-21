14f408fe63b837a4c3f4645d0e732ca823fabf03
Ivan Pesenti <ivan.pesenti.dev@gmail.com>
2022-09-21 15:27:23 +0200

    Point - red

--- PointOfSale/main_test.go
+++ PointOfSale/main_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+// TODO: requirement #3
 func TestScan_12345(t *testing.T) {
 	got := Scan("12345")
 	assert.Equal(t, "$7.25", got)
@@ -14,4 +15,10 @@ func TestScan_12345(t *testing.T) {
 func TestScan_23456(t *testing.T) {
 	got := Scan("23456")
 	assert.Equal(t, "$12.50", got)
+}
+
+func TestScan_99999(t *testing.T) {
+	got, err := Scan("99999")
+	assert.Equal(t, "$0.00", got)
+	assert.EqualError(t, err, "Error: barcode not found")
 }
