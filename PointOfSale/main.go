94f655b16f8902cc384db77182561a8f46c3ea0d
Ivan Pesenti <ivan.pesenti.dev@gmail.com>
2022-09-21 16:30:06 +0200

    Pos - refactor

--- PointOfSale/main.go
+++ PointOfSale/main.go
@@ -2,15 +2,16 @@ package pointofsale
 
 import "fmt"
 
+const ErrBarcodeNotFound = "barcode not found"
+
 var products = map[string]float64{
 	"12345": 7.25,
 	"23456": 12.5,
 }
 
-func Scan(barcode string) (string, error) {
-	if barcode == "99999" {
-		return fmt.Sprintf("$%.2f", 0.0), fmt.Errorf("Error: barcode not found")
+func Scan(barcode string) (price string, err error) {
+	if price, found := products[barcode]; found {
+		return fmt.Sprintf("$%.2f", price), nil
 	}
-	price, _ := products[barcode]
-	return fmt.Sprintf("$%.2f", price), nil
+	return fmt.Sprintf("$%.2f", 0.0), fmt.Errorf("error: %v", ErrBarcodeNotFound)
 }
--- PointOfSale/main_test.go
+++ PointOfSale/main_test.go
@@ -1,24 +1,48 @@
 package pointofsale
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-// TODO: requirement #3
-func TestScan_12345(t *testing.T) {
-	got, _ := Scan("12345")
-	assert.Equal(t, "$7.25", got)
-}
-
-func TestScan_23456(t *testing.T) {
-	got, _ := Scan("23456")
-	assert.Equal(t, "$12.50", got)
-}
-
-func TestScan_99999(t *testing.T) {
-	got, err := Scan("99999")
-	assert.Equal(t, "$0.00", got)
-	assert.EqualError(t, err, "Error: barcode not found")
+func TestScan(t *testing.T) {
+	test_suite := []struct {
+		name   string
+		input  string
+		output string
+		err    error
+	}{
+		{
+			name:   "MustReturn$7.25_WhenInputIs12345",
+			input:  "12345",
+			output: "$7.25",
+			err:    nil,
+		},
+		{
+			name:   "MustReturn$12.50_WhenInputIs23456",
+			input:  "23456",
+			output: "$12.50",
+			err:    nil,
+		},
+		{
+			name:   "MustReturnBarcodeNotFound_WhenInputNotExists",
+			input:  "99999",
+			output: "$0.00",
+			err:    fmt.Errorf("error: %v", ErrBarcodeNotFound),
+		},
+	}
+	for _, tt := range test_suite {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := Scan(tt.input)
+			if tt.err != nil {
+				assert.Equal(t, "$0.00", got)
+				assert.EqualError(t, err, tt.err.Error())
+				return
+			}
+			assert.Equal(t, tt.output, got)
+			assert.Nil(t, err)
+		})
+	}
 }
