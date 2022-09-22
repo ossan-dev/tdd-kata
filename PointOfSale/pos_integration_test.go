ab96ea041dd29668ab655ed6a0141237d93e31cf
Ivan Pesenti <ivan.pesenti.dev@gmail.com>
2022-09-22 09:54:40 +0200

    Point - add second integration test

--- PointOfSale/pos_integration_test.go
+++ PointOfSale/pos_integration_test.go
@@ -1,6 +1,3 @@
-//go:build integration
-// +build integration
-
 package pointofsale
 
 import (
@@ -20,4 +17,21 @@ func TestShoppingCart_SingleItem_ZeroAsStartAmount(t *testing.T) {
 	}
 	got := sut.GetTotal()
 	assert.Equal(t, "$12.50", got)
+}
+
+func TestShoppingCart_TwoItems_ZeroAsStartAmount(t *testing.T) {
+	sut, err := NewShoppingCart(0.00)
+	if err != nil {
+		t.Fatalf("failed to setup shopping_cart: %v", err)
+	}
+	_, err = sut.Scan("23456")
+	if err != nil {
+		t.Fatalf("failed to scan: %v", err)
+	}
+	_, err = sut.Scan("12345")
+	if err != nil {
+		t.Fatalf("failed to scan: %v", err)
+	}
+	got := sut.GetTotal()
+	assert.Equal(t, "$19.75", got)
 }
